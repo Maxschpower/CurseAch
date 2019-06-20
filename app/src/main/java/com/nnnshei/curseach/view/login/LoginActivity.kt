213c9a3934c4a4a372ca55d8d013e58c3a833ae6
Maxsc <tnnbbpe@gmail.com>
2019-06-20 21:02:13 +0500

    more shit

--- app/src/main/java/com/nnnshei/curseach/view/login/LoginActivity.kt
+++ app/src/main/java/com/nnnshei/curseach/view/login/LoginActivity.kt
@@ -1,15 +1,14 @@
 package com.nnnshei.curseach.view.login
 
 import android.os.Bundle
+import android.widget.Toast
 import com.arellomobile.mvp.MvpAppCompatActivity
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
 import com.nnnshei.curseach.R
 import com.nnnshei.curseach.presentation.login.LoginPresenter
 import com.nnnshei.curseach.presentation.login.LoginView
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlinx.android.synthetic.main.activity_login.*
 import org.koin.android.ext.android.get
 
 class LoginActivity : MvpAppCompatActivity(), LoginView {
@@ -23,6 +22,12 @@ class LoginActivity : MvpAppCompatActivity(), LoginView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        
+        button.setOnClickListener {
+            presenter.buttonClicked()
+        }
+    }
+
+    override fun onLoginClicked() {
+        Toast.makeText(this, text_input_nickname.toString(), Toast.LENGTH_SHORT).show()
     }
 }
